baf6c4ef68effaa6e59668c07c46f25787e2aeda
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-02 00:14:34 +0200

    style: add TaskSafeUpdateObject type

--- store/types.ts
+++ store/types.ts
@@ -1,5 +1,14 @@
-export type Task = {
+type TaskMandatoryFields = {
   id: string;
+};
+
+type TaskEditableFields = {
   title: string;
   checked: boolean;
 };
+
+export type Task = TaskMandatoryFields & TaskEditableFields;
+
+export type TaskSafeUpdateObject = {
+  [K in keyof TaskEditableFields]: TaskEditableFields[K] | undefined;
+} & TaskMandatoryFields;
