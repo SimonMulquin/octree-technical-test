ea066c2acffa393d934adbd654883bcaecaeb2e6
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-01 20:03:56 +0200

    feat: add SearchAppBar to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import SearchAppBar from "../components/SearchAppBar";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <SearchAppBar />
       <main className={styles.main}>Hello world</main>
     </div>
   );
