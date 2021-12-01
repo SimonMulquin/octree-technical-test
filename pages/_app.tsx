a23968450e9fbe690486e647e6fa1ded0dee97d3
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-01 18:48:17 +0200

    feat: add material-ui styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import app from "next/app";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../constants/theme";
+
+// Quick setup for material-ui and next-js see: https://itnext.io/next-js-with-material-ui-7a7f6485f671
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default app;
