858ec7856e2bd9630317f259dc90c12282991cff
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-01 20:34:18 +0200

    feat: add TasksList to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import SearchAppBar from "../components/SearchAppBar";
+import TasksList from "../components/TasksList";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <SearchAppBar />
-      <main className={styles.main}>Hello world</main>
+      <main className={styles.main}>
+        <TasksList />
+      </main>
     </div>
   );
 };
