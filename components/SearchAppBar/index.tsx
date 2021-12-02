10774c1a79774e5976d84cddf5aca9d5ab44cb6e
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-02 16:02:05 +0200

    feat: add margin after SearchAppBar

--- components/SearchAppBar/index.tsx
+++ components/SearchAppBar/index.tsx
@@ -15,6 +15,7 @@ import SearchIcon from "@material-ui/icons/Search";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginBottom: theme.spacing(2)
   },
   menuButton: {
     marginRight: theme.spacing(2),
