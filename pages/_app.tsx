c0de14e4108dd4481f172fa87e8822d81c04b895
Simon Mulquin <simon.mulquin@gmail.com>
2021-12-03 19:51:39 +0200

    feat: remove useless css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,12 @@ import React from "react";
 import app from "next/app";
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../constants/theme";
 
 // Quick setup for material-ui and next-js see: https://itnext.io/next-js-with-material-ui-7a7f6485f671
 
 function MyApp({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <React.Fragment>
       <Head>
@@ -31,7 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
